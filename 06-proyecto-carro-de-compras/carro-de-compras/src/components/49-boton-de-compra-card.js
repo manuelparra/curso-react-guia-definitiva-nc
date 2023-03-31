8034bf2272a781c71dc067485078968f71725dca
Manuel Parra <manuelparra@live.com.ar>
2023-03-31 16:24:24 +0200

    Agregando botton de compra

---
+++ 06-proyecto-carro-de-compras/carro-de-compras/src/components/49-boton-de-compra-card.js
@@ -0,0 +1,35 @@
+import { Component } from 'react'
+import BubbleAlert from './BubbleAlert'
+
+const styles = {
+  card: {
+    backgroundColor: '#359A2C',
+    color: '#fff', 
+    border: 'none', 
+    padding: '15px', 
+    borderRadius: '15px',
+    cursor: 'pointer',
+  }, 
+  bubble: {
+    position: 'relative', 
+    left: 12, 
+    top: 20,
+  }
+}
+
+class Card extends Component {
+  render() {
+    return (
+      <div>
+        <span style={styles.bubble}>
+          <BubbleAlert />
+        </span>
+        <button style={styles.card}>
+          Carro
+        </button>
+      </div>
+    )
+  }
+}
+
+export default Card
