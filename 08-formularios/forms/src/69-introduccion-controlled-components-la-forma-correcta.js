988e974a42f452c39b305aa7c9fbbbf15014ebe1
Manuel Parra <manuelparra@live.com.ar>
2023-05-05 15:49:38 +0200

    Sincronizando múlsltiples valores

---
+++ 08-formularios/forms/src/69-introduccion-controlled-components-la-forma-correcta.js
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [value, setValue] = useState('')
+  const handleChange = (e) => {
+    setValue(e.target.value)
+  }
+  console.log(value)
+
+  return (
+    <div>
+      {value.length < 5 ? <span>Longitud mínima de 5</span> : null}
+      <input type='text' name='normal' value={value} onChange={handleChange} />
+    </div>
+  )
+}
+
+export default App
--- 08-formularios/forms/src/App.js
+++ 08-formularios/forms/src/App.js
@@ -1,16 +1,20 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [value, setValue] = useState('')
+  const [value, setValue] = useState({ normal: 'Por defecto', texto: '' })
   const handleChange = (e) => {
-    setValue(e.target.value)
+    setValue((state) => ({
+      ...state, 
+      [e.target.name]: e.target.value 
+    }))
   }
   console.log(value)
 
   return (
     <div>
       {value.length < 5 ? <span>Longitud mínima de 5</span> : null}
-      <input type='text' name='normal' value={value} onChange={handleChange} />
+      <input type='text' name='normal' value={value.normal} onChange={handleChange} />
+      <textarea name='texto' value={value.texto} onChange={handleChange} />
     </div>
   )
 }
