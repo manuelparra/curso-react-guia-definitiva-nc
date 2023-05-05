696d3314dbc2cc9c9b44555bca0f03ad4d7ec4fa
Manuel Parra <manuelparra@live.com.ar>
2023-05-05 16:06:07 +0200

    Componente Checkbox

--- 08-formularios/forms/src/App.js
+++ 08-formularios/forms/src/App.js
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [value, setValue] = useState({ normal: 'Por defecto', texto: '' })
-  const handleChange = (e) => {
+  const [value, setValue] = useState({ normal: 'Por defecto', texto: '', select: '', check: false })
+  const handleChange = ({ target }) => {
+    console.log(target.type, target.checked)
     setValue((state) => ({
       ...state, 
-      [e.target.name]: e.target.value 
+      [target.name]: target.type === 'checkbox' ? target.checked : target.value 
     }))
   }
   console.log(value)
@@ -15,6 +16,20 @@ const App = () => {
       {value.length < 5 ? <span>Longitud mínima de 5</span> : null}
       <input type='text' name='normal' value={value.normal} onChange={handleChange} />
       <textarea name='texto' value={value.texto} onChange={handleChange} />
+
+      <select value={value.select} name='select' onChange={handleChange}>
+        <option value=''>-- Seleccione --</option>
+        <option value='Chancho feliz'>Chancho feliz</option>
+        <option value='Chanchito feliz'>Chanchito feliz</option>
+        <option value='Chanchito triste'>Chanchito triste</option>
+      </select>
+
+      <input 
+        type="checkbox"
+        name="check"
+        onChange={handleChange}
+        checked={value.check} />
+      
     </div>
   )
 }
