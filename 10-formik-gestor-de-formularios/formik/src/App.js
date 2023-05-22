1e993762cf4b67617bef5a51b386b3c0f7ac7f89
Manuel Parra <manuelparra@live.com.ar>
2023-05-22 16:48:31 +0200

    Agregando validaciones

--- 10-formik-gestor-de-formularios/formik/src/App.js
+++ 10-formik-gestor-de-formularios/formik/src/App.js
@@ -7,6 +7,27 @@ const App = () => {
       lastname: '', 
       email: '',
     },
+    validate: (values) => {
+      const errors = {} 
+
+      if(!values.name) {
+        errors.name = 'Requerido'
+      } else if(values.name.length < 5) {
+        errors.name = 'El nombre es muy corto'
+      }
+
+      if(!values.lastname) {
+        errors.lastname = 'Requerido'
+      } else if(values.lastname.length < 5) {
+        errors.lastname = 'El apellido es muy corto'
+      }
+    
+      if(!values.email) {
+        errors.email = 'Requerido'
+      }
+
+      return errors
+    },
     onSubmit: values => console.log(values)
   })
 
@@ -19,6 +40,7 @@ const App = () => {
         onChange={formik.handleChange}
         value={formik.values.name}
       />
+      {formik.errors.name ? <div>{formik.errors.name}</div> : null}
       <br />
       <label>Apellido</label>
       <input 
@@ -27,6 +49,7 @@ const App = () => {
         onChange={formik.handleChange}
         value={formik.values.lastname}
       />
+      {formik.errors.lastname ? <div>{formik.errors.lastname}</div> : null}
       <br />
       <label>Email</label>
       <input 
@@ -35,6 +58,7 @@ const App = () => {
         onChange={formik.handleChange}
         value={formik.values.email}
       />
+      {formik.errors.email ? <div>{formik.errors.email}</div> : null}
       <br />
       <button type="submit">Enviar</button>
     </form>
