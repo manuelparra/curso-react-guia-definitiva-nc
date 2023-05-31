1e030be1091a317efba17b99eeab1df73bfa62f1
Manuel Parra <manuelparra@live.com.ar>
2023-06-01 00:08:31 +0200

    Componente Field, Form y ErrorMessage

--- 10-formik-gestor-de-formularios/formik/src/App.js
+++ 10-formik-gestor-de-formularios/formik/src/App.js
@@ -1,4 +1,4 @@
-import { Formik } from 'formik'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
 
 const validate = (values) => {
   const errors = {} 
@@ -29,29 +29,21 @@ const App = () => {
       validate={validate}
       onSubmit={values => console.log(values)}
     >
-      {formik => 
-        <form onSubmit={formik.handleSubmit}>
-          <label>Nombre</label>
-          <input 
-            type='text' {...formik.getFieldProps('name')}
-          />
-          {formik.touched.name && formik.errors.name ? <div>{formik.errors.name}</div> : null}
-          <br />
-          <label>Apellido</label>
-          <input 
-            type='text' {...formik.getFieldProps('lastname')} 
-          />
-          {formik.touched.lastname && formik.errors.lastname ? <div>{formik.errors.lastname}</div> : null}
-          <br />
-          <label>Email</label>
-          <input 
-            type='text' {...formik.getFieldProps('email')} 
-          />
-          {formik.touched.email && formik.errors.email ? <div>{formik.errors.email}</div> : null}
-          <br />
-          <button type="submit">Enviar</button>
-        </form>
-      }
+      <Form>
+        <label>Nombre</label>
+        <Field name="name" type="text" />
+        <ErrorMessage name="name" />
+        <br />
+        <label>Apellido</label>
+        <Field name="lastname" type="text" />
+        <ErrorMessage name="lastname" />
+        <br />
+        <label>Email</label>
+        <Field name="email" type="text" />
+        <ErrorMessage name="email" />   
+        <br />
+        <button type="submit">Enviar</button>
+      </Form>
     </Formik>
   );
 }
