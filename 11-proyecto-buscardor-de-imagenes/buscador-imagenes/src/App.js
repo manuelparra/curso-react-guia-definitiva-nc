14f4f44544a9c9bff2e1993e0ca638e5e708d8b4
Manuel Parra <manuelparra@live.com.ar>
2023-06-07 00:00:58 +0200

    Llamando a la API

--- 11-proyecto-buscardor-de-imagenes/buscador-imagenes/src/App.js
+++ 11-proyecto-buscardor-de-imagenes/buscador-imagenes/src/App.js
@@ -1,7 +1,10 @@
+import { useState } from 'react'
 import { Formik, Form, Field } from 'formik'
 import './header.css'
 
 const App = () => {
+  const [photos, setPhotos] = useState([])
+  console.log(photos)
   return (
     <div>
       <header>
@@ -9,7 +12,13 @@ const App = () => {
           initialValues={{ search: '' }}
           onSubmit={async values => {
             // llamar a api de unsplash
-            console.log(values)
+            const response = await fetch(`https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`, {
+              headers: {
+                'Authorization': 'Client-ID 9or8rL6V5TB_aHq9E7faPw_oMEySjo2AgpR6Q7-k-KE'
+              }
+            })
+            const data = await response.json()
+            setPhotos(data.results)
           }}
         >
           <Form>
