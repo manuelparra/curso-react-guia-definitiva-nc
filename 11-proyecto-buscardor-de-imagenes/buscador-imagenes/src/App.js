f83aafcba3030b5d7c6522c328694985c6b776a9
Manuel Parra <manuelparra@live.com.ar>
2023-06-16 21:48:45 +0200

    Agregue un content leader

--- 11-proyecto-buscardor-de-imagenes/buscador-imagenes/src/App.js
+++ 11-proyecto-buscardor-de-imagenes/buscador-imagenes/src/App.js
@@ -1,29 +1,35 @@
 import { useState } from 'react'
 import { Formik, Form, Field } from 'formik'
+import ArticleItem from './components/ArticleItem'
+import ArticleItemPlaceholder from './components/ArticleItemPlaceholder' 
 import './header.css'
 import './content.css'
 import './article.css'
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(false)
   const [photos, setPhotos] = useState([])
-  const open = url => window.open(url)
 
-  console.log(photos)
+  const handleSubmit = async(values) => {
+    // set loading variable to true
+    setIsLoading(true)
+    // llamar a api de unsplash
+    const response = await fetch(`https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`, {
+      headers: {
+        'Authorization': 'Client-ID 9or8rL6V5TB_aHq9E7faPw_oMEySjo2AgpR6Q7-k-KE'
+      }
+    })
+    const data = await response.json()
+    setPhotos(data.results)
+    setIsLoading(false)
+  }
+
   return (
     <div>
       <header>
         <Formik
           initialValues={{ search: '' }}
-          onSubmit={async values => {
-            // llamar a api de unsplash
-            const response = await fetch(`https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`, {
-              headers: {
-                'Authorization': 'Client-ID 9or8rL6V5TB_aHq9E7faPw_oMEySjo2AgpR6Q7-k-KE'
-              }
-            })
-            const data = await response.json()
-            setPhotos(data.results)
-          }}
+          onSubmit={handleSubmit}
         >
           <Form>
             <Field name="search" />
@@ -32,21 +38,16 @@ const App = () => {
       </header>
       <div className="container">
         <div className="center">
-          {photos.map(photo => 
-            <article key={photo.id} onClick={() => open(photo.links.html)}>
-              <img src={photo.urls.regular} alt={photo.description} />
-              <p>
-                {photo.description == null && photo.alt_description == null ?
-                  "There is not description for this picture" :
-                  photo.description == null ? 
-                    photo.alt_description[0].toUpperCase() + photo.alt_description.substring(1) : 
-                    photo.alt_description == null ? 
-                      photo.description[0].toUpperCase() + photo.description.substring(1) : 
-                      [photo.description[0].toUpperCase() + photo.description.substring(1), photo.alt_description[0].toUpperCase() + photo.alt_description.substring(1)].join(' - ')
-                }  
-              </p>
-            </article>
-          )}
+          {isLoading && 
+            photos.map(() =>
+              <ArticleItemPlaceholder />
+            )
+          }
+          {!isLoading && 
+            photos.map(photo => 
+              <ArticleItem photo={photo} /> 
+            )
+          }
         </div>
       </div>
     </div>
---
+++ 11-proyecto-buscardor-de-imagenes/buscador-imagenes/src/components/ArticleItem.js
@@ -0,0 +1,21 @@
+const ArticleItem = ({ photo }) => {
+  const open = url => window.open(url)
+  
+  return (
+    <article key={photo.id} onClick={() => open(photo.links.html)}>
+      <img src={photo.urls.regular} alt={photo.description} />
+      <p>
+        {photo.description == null && photo.alt_description == null ?
+          "There is not description for this picture" :
+          photo.description == null ? 
+            photo.alt_description[0].toUpperCase() + photo.alt_description.substring(1) : 
+            photo.alt_description == null ? 
+              photo.description[0].toUpperCase() + photo.description.substring(1) : 
+              [photo.description[0].toUpperCase() + photo.description.substring(1), photo.alt_description[0].toUpperCase() + photo.alt_description.substring(1)].join(' - ')
+        } 
+      </p>
+    </article>
+  )
+}
+
+export default ArticleItem
