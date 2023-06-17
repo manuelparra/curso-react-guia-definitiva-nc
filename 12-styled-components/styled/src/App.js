4efe683b80be10fbfea79805afba6fc34ba4de62
Manuel Parra <manuelparra@live.com.ar>
2023-06-17 20:33:44 +0200

    Styled-Components, composición de componentes

--- 12-styled-components/styled/src/App.js
+++ 12-styled-components/styled/src/App.js
@@ -4,11 +4,15 @@ const P = styled.p`
   font-size: 24px;
   color: red;
 `
+const Content = styled.div`
+  padding: 20px 25px;
+`
+
 const App = () => {
   return (
-    <div>
+    <Content>
       <P>Hola soy un parrafo</P>
-    </div> 
+    </Content> 
   )
 }
 
