383a627bb75220a25bcbf772050d79ca3ed5c8ee
Manuel Parra <manuelparra@live.com.ar>
2023-06-17 20:43:48 +0200

    Styled-Components, adaptándese en base a las propiedades

--- 12-styled-components/styled/src/App.js
+++ 12-styled-components/styled/src/App.js
@@ -4,14 +4,26 @@ const P = styled.p`
   font-size: 24px;
   color: red;
 `
+
 const Content = styled.div`
   padding: 20px 25px;
+`
+
+const Button = styled.button`
+  background-color: ${props => props.primary ? 'red' : 'white'};
+  color: ${props => props.primary ? 'white' : 'red'};
+  padding: 10px 15px;
+  border: solid 2pk red;
+  border-radius: 4px;
+  cursor: pointer;
 `
 
 const App = () => {
   return (
     <Content>
       <P>Hola soy un parrafo</P>
+      <Button>Enviar</Button>
+      <Button primary={true}>Enviar</Button>
     </Content> 
   )
 }
