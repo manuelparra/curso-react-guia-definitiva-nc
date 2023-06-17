134bd6ff7693569df16e455b76dd99a7648d4de4
Manuel Parra <manuelparra@live.com.ar>
2023-06-17 20:29:49 +0200

    Styled-Components, instalación y funcionamiento

---
+++ 12-styled-components/styled/src/App.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+const P = styled.p`
+  font-size: 24px;
+  color: red;
+`
+const App = () => {
+  return (
+    <div>
+      <P>Hola soy un parrafo</P>
+    </div> 
+  )
+}
+
+export default App
