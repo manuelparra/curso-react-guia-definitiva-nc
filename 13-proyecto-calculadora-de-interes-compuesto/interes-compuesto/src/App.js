3655dc69f9247ced7fee5fdb9fecc88f1e2910bb
Manuel Parra <manuelparra@live.com.ar>
2023-06-26 21:55:01 +0200

    Creando un contenedor

---
+++ 13-proyecto-calculadora-de-interes-compuesto/interes-compuesto/src/App.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 100%;
+  align-items: center;
+`
+
+const App = () => {
+  return (
+    <Container>
+      lala
+    </Container>
+  )
+}
+
+export default App
