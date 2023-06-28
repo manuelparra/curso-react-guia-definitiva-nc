4f579eef5aa5f083dc251f1a137243824cfe6e3c
Manuel Parra <manuelparra@live.com.ar>
2023-06-28 22:01:58 +0200

    Componente de Button y refactorización

--- 13-proyecto-calculadora-de-interes-compuesto/interes-compuesto/src/App.js
+++ 13-proyecto-calculadora-de-interes-compuesto/interes-compuesto/src/App.js
@@ -1,20 +1,8 @@
-import styled from 'styled-components'
 import { Formik, Form } from 'formik'
 import Input from './components/Input'
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  height: 100%;
-  align-items: center;
-`
-
-const Section = styled.section`
-  background-color: #eee;
-  border-top: solid 2px palevioletred;
-  padding: 20px 25px;
-  width: 500px;
-  box-shadow: 0px 2px 3px rgb(0, 0, 0, 0.3);
-`
+import Button from './components/Button'
+import Container from './components/Container'
+import Section from './components/Section'
 
 const App = () => {
   const handleSubmit = () => {}
@@ -35,6 +23,7 @@ const App = () => {
             <Input name="contribution" label="Contribución anual" />
             <Input name="years" label="Años" />
             <Input name="rate" label="Interes estimado" />
+            <Button>Calcular</Button>
           </Form>
         </Formik>
       </Section>
