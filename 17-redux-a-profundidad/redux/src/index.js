1648607709228bdbcf24766aca98eea4aaf4750d
Manuel Parra <manuelparra@live.com.ar>
2023-09-23 18:36:43 +0200

    148 - Conectando con React

--- 17-redux-a-profundidad/redux/src/index.js
+++ 17-redux-a-profundidad/redux/src/index.js
@@ -1,39 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {  createStore } from 'redux'
-import App from './App'
+import { Provider } from 'react-redux'
+import App, { reducer } from './App'
 import reportWebVitals from './reportWebVitals';
 
-const store = createStore((state = 0, action) => { // esta función es un reducer
-  switch (action.type) {
-    case 'incrementar': {
-      return state + 1
-    }
-    case 'decrementar': {
-      return state - 1
-    }
-    case 'set': {
-      return action.payload
-    }
-    default: 
-      return state
-  }
-})
-
-console.log(store.getState())
-store.dispatch({ type: 'lala' })
-console.log(store.getState())
-store.dispatch({ type: 'incrementar' })
-console.log(store.getState())
-store.dispatch({ type: 'decrementar' })
-console.log(store.getState())
-store.dispatch({ type: 'set', payload: 15 })
-console.log(store.getState())
+const store = createStore(reducer)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 //  <React.StrictMode>
+  <Provider store={store}>
     <App />
+  </Provider>   
 //  </React.StrictMode>
 );
 
