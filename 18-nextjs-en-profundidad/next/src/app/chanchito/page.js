1dead5fe012aa4301dcd1fd34d15a5c1507fd6e4
Manuel Parra <manuelparra@live.com.ar>
2023-10-14 16:39:35 +0200

    165 - Páginas, Link y code splitting

---
+++ 18-nextjs-en-profundidad/next/src/app/chanchito/page.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+const Chanchito = () => {
+  return (
+    <dev>
+      <p>Chanchito Triste</p>
+      <Link href="/">Inicio</Link>
+    </dev>
+  )
+}
+
+export default Chanchito
---
+++ 18-nextjs-en-profundidad/next/src/app/page.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+const Home = () => {
+  return (
+    <dev>
+      <p>Chanchito Feliz</p> 
+      <Link href="/chanchito">Chanchito Triste</Link>
+    </dev> 
+  )
+}
+
+export default Home
